54f3d26681c250e9fa2cd31dbdcded549af4ce39
風は吹いてるか? <50514886+rinscr3003@users.noreply.github.com>
2023-05-11 16:56:47 +0800

    舵机测试

--- EmmmCar-Center/main.c
+++ EmmmCar-Center/main.c
@@ -38,6 +38,29 @@ OF SUCH DAMAGE.
 #include "gd32f30x.h"
 #include "systick.h"
 #include <stdio.h>
+#include <stdint.h>
+
+void gd_log_com_init()
+{
+    /* enable COM GPIO clock */
+    rcu_periph_clock_enable(RCU_GPIOA);
+
+    /* enable USART clock */
+    rcu_periph_clock_enable(RCU_USART0);
+
+    /* connect port to USARTx_Tx */
+    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
+
+    /* connect port to USARTx_Rx */
+    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
+
+    /* USART configure */
+    usart_deinit(USART0);
+    usart_baudrate_set(USART0, 115200U);
+    usart_receive_config(USART0, USART_RECEIVE_ENABLE);
+    usart_transmit_config(USART0, USART_TRANSMIT_ENABLE);
+    usart_enable(USART0);
+}
 
 /*!
     \brief      main function
@@ -52,7 +75,42 @@ int main(void)
     /* configure systick */
     systick_config();
     /* initilize the LEDs, USART and key */
+	
+	
+rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_AF);
+    gpio_init(GPIOA,GPIO_MODE_AF_PP,GPIO_OSPEED_50MHZ,GPIO_PIN_6);
+	timer_oc_parameter_struct timer_ocintpara;
+    timer_parameter_struct timer_initpara;
+    rcu_periph_clock_enable(RCU_TIMER2);
+    timer_deinit(TIMER2);
+    timer_initpara.prescaler         = 119;
+    timer_initpara.alignedmode       = TIMER_COUNTER_EDGE;
+    timer_initpara.counterdirection  = TIMER_COUNTER_UP;
+    timer_initpara.period            = 4999;
+    timer_initpara.clockdivision     = TIMER_CKDIV_DIV1;
+    timer_initpara.repetitioncounter = 0;
+    timer_init(TIMER2,&timer_initpara);
 
+    /* CH0,CH1 and CH2 configuration in PWM mode */
+    timer_ocintpara.outputstate  = TIMER_CCX_ENABLE;
+    timer_ocintpara.outputnstate = TIMER_CCXN_DISABLE;
+    timer_ocintpara.ocpolarity   = TIMER_OC_POLARITY_HIGH;
+    timer_ocintpara.ocnpolarity  = TIMER_OCN_POLARITY_HIGH;
+    timer_ocintpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
+    timer_ocintpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
+
+    timer_channel_output_config(TIMER2,TIMER_CH_0,&timer_ocintpara);
+
+    /* CH0 configuration in PWM mode0,duty cycle 25% */
+    timer_channel_output_pulse_value_config(TIMER2,TIMER_CH_0,1500);
+    timer_channel_output_mode_config(TIMER2,TIMER_CH_0,TIMER_OC_MODE_PWM0);
+    timer_channel_output_shadow_config(TIMER2,TIMER_CH_0,TIMER_OC_SHADOW_ENABLE);
+
+    /* auto-reload preload enable */
+    timer_auto_reload_shadow_enable(TIMER2);
+    /* auto-reload preload enable */
+    timer_enable(TIMER2);
     
     /* print out the clock frequency of system, AHB, APB1 and APB2 */
     printf("\r\nCK_SYS is %d", rcu_clock_freq_get(CK_SYS));
@@ -60,8 +118,17 @@ int main(void)
     printf("\r\nCK_APB1 is %d", rcu_clock_freq_get(CK_APB1));
     printf("\r\nCK_APB2 is %d", rcu_clock_freq_get(CK_APB2));
 
+delay_1ms(2000);
+
     while (1){
-        ;
+        for(uint16_t i=500;i<2500;i++){
+					timer_channel_output_pulse_value_config(TIMER2,TIMER_CH_0,i);
+					delay_1ms(1);
+				}
+				for(uint16_t i=2500;i>500;i--){
+					timer_channel_output_pulse_value_config(TIMER2,TIMER_CH_0,i);
+					delay_1ms(1);
+				}
     }
 }
 
