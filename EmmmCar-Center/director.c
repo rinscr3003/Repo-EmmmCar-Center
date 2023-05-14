a04fb92dd3728c406d860c27ced7403a7de10975
風は吹いてるか? <50514886+rinscr3003@users.noreply.github.com>
2023-05-14 16:23:02 +0800

    两种标准巡线方式建立完成

--- EmmmCar-Center/director.c
+++ EmmmCar-Center/director.c
@@ -3,6 +3,7 @@
 #include "bsp_fdetect.h"
 #include "bsp_linefind.h"
 #include "bsp_spi.h"
+#include "systick.h"
 
 /*
  * Director
@@ -13,11 +14,11 @@
 
 void Director_Init(Director_ThisState *thisState, Director_PosState posState)
 {
-    thisState->lastState = POS_UNDEFINED;
-    thisState->nowState = posState;
-    thisState->nextState = POS_UNDEFINED;
-    thisState->lastLoopSide = LOOP_RIGHT;
-    thisState->steps = 0;
+  thisState->lastState = POS_UNDEFINED;
+  thisState->nowState = posState;
+  thisState->nextState = POS_UNDEFINED;
+  thisState->lastLoopSide = LOOP_RIGHT;
+  thisState->steps = 0;
 }
 
 const static Director_PosState _route[] = {
@@ -80,25 +81,136 @@ const static Director_PosState _route[] = {
 
 void _Director_PosShift(Director_ThisState *thisState, Director_PosState targetState)
 {
-    thisState->lastState = thisState->nowState;
-    thisState->nowState = thisState->nextState;
-    thisState->nextState = targetState;
+  thisState->lastState = thisState->nowState;
+  thisState->nowState = thisState->nextState;
+  thisState->nextState = targetState;
+}
+
+void _Director_NormalLineFollow()
+{
+  BSP_LineFind_Result lineRes = BSP_Linefind_Read();
+  if (lineRes.R3 == 1 && lineRes.R2 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 600, 0);
+    BSP_SPI_SetMotorPWM(1, 350, 0);
+    BSP_SPI_SetMotorPWM(2, 600, 0);
+    BSP_SPI_SetMotorPWM(3, 350, 0);
+  }
+  else if (lineRes.L3 == 1 && lineRes.L2 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 350, 0);
+    BSP_SPI_SetMotorPWM(1, 600, 0);
+    BSP_SPI_SetMotorPWM(2, 350, 0);
+    BSP_SPI_SetMotorPWM(3, 600, 0);
+  }
+  else if (lineRes.R2 == 1 && lineRes.R1 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 550, 0);
+    BSP_SPI_SetMotorPWM(1, 400, 0);
+    BSP_SPI_SetMotorPWM(2, 550, 0);
+    BSP_SPI_SetMotorPWM(3, 400, 0);
+  }
+  else if (lineRes.L1 == 1 && lineRes.L2 == 1)
+  {
+
+    BSP_SPI_SetMotorPWM(0, 400, 0);
+    BSP_SPI_SetMotorPWM(1, 550, 0);
+    BSP_SPI_SetMotorPWM(2, 400, 0);
+    BSP_SPI_SetMotorPWM(3, 550, 0);
+  }
+  else if (lineRes.R4 == 1 && lineRes.R3 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 450, 0);
+    BSP_SPI_SetMotorPWM(1, 450, 1);
+    BSP_SPI_SetMotorPWM(2, 450, 0);
+    BSP_SPI_SetMotorPWM(3, 450, 1);
+  }
+  else if (lineRes.L4 == 1 && lineRes.L3 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 450, 1);
+    BSP_SPI_SetMotorPWM(1, 450, 0);
+    BSP_SPI_SetMotorPWM(2, 450, 1);
+    BSP_SPI_SetMotorPWM(3, 450, 0);
+  }
+  else
+  {
+
+    BSP_SPI_SetMotorPWM(0, 500, 0);
+    BSP_SPI_SetMotorPWM(1, 500, 0);
+    BSP_SPI_SetMotorPWM(2, 500, 0);
+    BSP_SPI_SetMotorPWM(3, 500, 0);
+  }
+  delay_1ms(10);
+}
+
+void _Director_AggressiveLineFollow()
+{
+  BSP_LineFind_Result lineRes = BSP_Linefind_Read();
+  if (lineRes.R4 == 1 && lineRes.R3 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 600, 0);
+    BSP_SPI_SetMotorPWM(1, 400, 1);
+    BSP_SPI_SetMotorPWM(2, 600, 0);
+    BSP_SPI_SetMotorPWM(3, 400, 1);
+  }
+  else if (lineRes.L4 == 1 && lineRes.L3 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 400, 1);
+    BSP_SPI_SetMotorPWM(1, 600, 0);
+    BSP_SPI_SetMotorPWM(2, 400, 1);
+    BSP_SPI_SetMotorPWM(3, 600, 0);
+  }
+  else if (lineRes.R3 == 1 && lineRes.R2 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 600, 0);
+    BSP_SPI_SetMotorPWM(1, 250, 0);
+    BSP_SPI_SetMotorPWM(2, 600, 0);
+    BSP_SPI_SetMotorPWM(3, 250, 0);
+  }
+  else if (lineRes.L3 == 1 && lineRes.L2 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 250, 0);
+    BSP_SPI_SetMotorPWM(1, 600, 0);
+    BSP_SPI_SetMotorPWM(2, 250, 0);
+    BSP_SPI_SetMotorPWM(3, 600, 0);
+  }
+  else if (lineRes.R2 == 1 && lineRes.R1 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 500, 0);
+    BSP_SPI_SetMotorPWM(1, 300, 0);
+    BSP_SPI_SetMotorPWM(2, 500, 0);
+    BSP_SPI_SetMotorPWM(3, 300, 0);
+  }
+  else if (lineRes.L1 == 1 && lineRes.L2 == 1)
+  {
+    BSP_SPI_SetMotorPWM(0, 300, 0);
+    BSP_SPI_SetMotorPWM(1, 500, 0);
+    BSP_SPI_SetMotorPWM(2, 300, 0);
+    BSP_SPI_SetMotorPWM(3, 500, 0);
+  }
+  else
+  {
+    BSP_SPI_SetMotorPWM(0, 400, 0);
+    BSP_SPI_SetMotorPWM(1, 400, 0);
+    BSP_SPI_SetMotorPWM(2, 400, 0);
+    BSP_SPI_SetMotorPWM(3, 400, 0);
+  }
 }
 
 void Director_Loop(Director_ThisState *thisState)
 {
-    switch (thisState->steps)
+  switch (thisState->steps)
+  {
+  case 0:
+
+    _Director_PosShift(thisState, _route[thisState->steps + 1]);
+    thisState->steps++;
+    break;
+
+  default:
+    while (1)
     {
-    case 0:
-
-        _Director_PosShift(thisState, _route[thisState->steps + 1]);
-        thisState->steps++;
-        break;
-
-    default:
-        while (1)
-        {
-            BSP_LEDBUZ_Flash(BSP_LEDBUZ_LED, 500, 100);
-        }
+      BSP_LEDBUZ_Flash(BSP_LEDBUZ_LED, 500, 100);
     }
+  }
 }
